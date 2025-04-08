b2eadd82fd0d25edd66c6fc7d0d2a5a9f756f405
leewatertrue <leewatertrue@gmail.com>
2025-04-08 22:53:13 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 176 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String an=s.nextLine();
+		if (an.equals("SONGDO")) {
+            System.out.println("HIGHSCHOOL");
+        } else if (an.equals("CODE")) {
+            System.out.println("MASTER");
+        } else if (an.equals("2023")) {
+            System.out.println("0611");
+        } else if (an.equals("ALGORITHM")) {
+            System.out.println("CONTEST");
+        }
+}}
