849ca9a6dd1e91be24ce9dd4e34dd8f23a794f1b
leewatertrue <leewatertrue@gmail.com>
2025-11-14 23:12:53 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,6 +1,4 @@
 def solution(numbers):
     numbers.sort()
-    answer = numbers[0]*numbers[1]
-    for n in range(len(numbers)-1):
-        if answer<numbers[n]*numbers[n+1]: answer=numbers[n]*numbers[n+1]
+    answer=numbers[-2]*numbers[-1]
     return answer
