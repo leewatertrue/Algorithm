232bee63829eb607a1e7ed9351365e6017d98511
leewatertrue <leewatertrue@gmail.com>
2026-01-25 23:07:51 +0900

    [level 1] Title: 숫자 짝꿍, Time: 466.72 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,14 +1,13 @@
 from collections import Counter
-def solution(x, y):
-    answer = ''
+def solution(x,y):
+    ans=[]
     xc=Counter(x)
     yc=Counter(y)
-    digits=[]
     for i in range(10):
         digit=str(i)
-        count=min(xc.get(digit,0), yc.get(digit,0))
-        digits.extend([digit]*count)
-    if not digits: return "-1"
-    if all (d=='0' for d in digits): return "0"
-    digits.sort(reverse=True)
-    return "".join(digits)
+        count=min(xc.get(digit,0), yc.get(digit, 0))
+        ans.extend(digit*count)
+    if not ans: return "-1"
+    if all (d=='0' for d in ans): return "0"
+    ans.sort(reverse=True)
+    return "".join(ans)
